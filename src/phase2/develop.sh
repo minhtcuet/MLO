b98c556bcdadcf9c8de88f92f641414362632f2d
minhtc <minhtc.uet@gmail.com>
2023-07-30 17:46:09 +0700

    fastapi

--- src/phase2/develop.sh
+++ src/phase2/develop.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=/home/project/app/kernel:$PYTHONPATH
-gunicorn -w 7 -b :5000 prediction:app
+#gunicorn -w 7 -b :5000 prediction:app
 
+uvicorn prediction:app --port 5000 --workers 14
--- src/phase2/prediction.py
+++ src/phase2/prediction.py
@@ -1,44 +1,136 @@
-import json
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-# import json_logging
-import numpy as np
-from loguru import logger
+# import json
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# # import json_logging
+# import numpy as np
+# from loguru import logger
+# from features.orchestrator import Orchestrator
+# import cython_code
+# import uuid
+#
+# app = Flask(__name__)
+# cors = CORS(app, resources={r'/api/*': {'origin': '*'}})
+#
+# orch = Orchestrator()
+# COLUMNS = ['feature{}'.format(i) for i in range(1, 42)]
+#
+#
+# @app.route("/phase-2/prob-1/predict", methods=['POST'])
+# def predict():
+#     ids, drift, res = None, 0, []
+#     try:
+#         data = request.get_json(force=True)
+#
+#         # with open('./log/problem1_{}.txt'.format(uuid.uuid4()), 'w') as f:
+#         #     f.write(str(data))
+#
+#         # logger.info("Get info")
+#         ids = data.get('id')
+#         columns = data.get('columns')
+#
+#         rows = cython_code.convert2numpyarr(data.get('rows'))
+#         # rows = np.asarray(data.get('rows'))
+#
+#         # logger.info("Reorder features")
+#         new_column_indexes = [columns.index(name) for name in COLUMNS]
+#         rows = rows[:, new_column_indexes]
+#
+#         res = orch.predict(data=rows, model='prob1')
+#         # logger.info("Return")
+#
+#         return jsonify(
+#             {
+#                 'id': ids,
+#                 'predictions': res,
+#                 'drift': 0
+#             }
+#         )
+#
+#     except Exception as e:
+#         logger.error(e)
+#         return jsonify(
+#             {
+#                 'id': ids,
+#                 'predictions': res,
+#                 'drift': 0
+#             }
+#         )
+#
+#
+# @app.route("/phase-2/prob-2/predict", methods=['POST'])
+# def predict_prob2():
+#     ids, drift, res = None, 0, []
+#     try:
+#         data = request.get_json(force=True)
+#
+#         # with open('./log/problem2_{}.txt'.format(uuid.uuid4()), 'w') as f:
+#         #     f.write(str(data))
+#
+#         # logger.info("Get info")
+#         ids = data.get('id')
+#         columns = data.get('columns')
+#
+#         rows = cython_code.convert2numpyarr(data.get('rows'))
+#         # rows = np.asarray(data.get('rows'))
+#
+#         # logger.info("Reorder features")
+#         new_column_indexes = [columns.index(name) for name in COLUMNS]
+#         rows = rows[:, new_column_indexes]
+#
+#         res = orch.predict(data=rows, model='prob2')
+#         # logger.info("Return")
+#         return jsonify(
+#             {
+#                 'id': ids,
+#                 'predictions': res,
+#                 'drift': 0
+#             }
+#         )
+#
+#     except Exception as e:
+#         logger.error(e)
+#         return jsonify(
+#             {
+#                 'id': ids,
+#                 'predictions': res,
+#                 'drift': 0
+#             }
+#         )
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from features.orchestrator import Orchestrator
+from loguru import logger
 import cython_code
-import uuid
 
-app = Flask(__name__)
-cors = CORS(app, resources={r'/api/*': {'origin': '*'}})
 
+app = FastAPI()
 orch = Orchestrator()
 COLUMNS = ['feature{}'.format(i) for i in range(1, 42)]
 
 
-@app.route("/phase-2/prob-1/predict", methods=['POST'])
-def predict():
+@app.post("/phase-2/prob-1/predict")
+async def predict(request: Request):
     ids, drift, res = None, 0, []
     try:
-        data = request.get_json(force=True)
+        data = await request.json()
 
-        # with open('./log/problem1_{}.txt'.format(uuid.uuid4()), 'w') as f:
-        #     f.write(str(data))
-
-        # logger.info("Get info")
         ids = data.get('id')
         columns = data.get('columns')
 
         rows = cython_code.convert2numpyarr(data.get('rows'))
-        # rows = np.asarray(data.get('rows'))
 
-        # logger.info("Reorder features")
         new_column_indexes = [columns.index(name) for name in COLUMNS]
         rows = rows[:, new_column_indexes]
 
         res = orch.predict(data=rows, model='prob1')
-        # logger.info("Return")
 
-        return jsonify(
+        return JSONResponse(
             {
                 'id': ids,
                 'predictions': res,
@@ -48,7 +140,7 @@ def predict():
 
     except Exception as e:
         logger.error(e)
-        return jsonify(
+        return JSONResponse(
             {
                 'id': ids,
                 'predictions': res,
@@ -57,29 +149,23 @@ def predict():
         )
 
 
-@app.route("/phase-2/prob-2/predict", methods=['POST'])
-def predict_prob2():
+@app.post("/phase-2/prob-2/predict")
+async def predict_prob2(request: Request):
     ids, drift, res = None, 0, []
     try:
-        data = request.get_json(force=True)
+        data = await request.json()
 
-        # with open('./log/problem2_{}.txt'.format(uuid.uuid4()), 'w') as f:
-        #     f.write(str(data))
-
-        # logger.info("Get info")
         ids = data.get('id')
         columns = data.get('columns')
 
         rows = cython_code.convert2numpyarr(data.get('rows'))
-        # rows = np.asarray(data.get('rows'))
 
-        # logger.info("Reorder features")
         new_column_indexes = [columns.index(name) for name in COLUMNS]
         rows = rows[:, new_column_indexes]
 
         res = orch.predict(data=rows, model='prob2')
-        # logger.info("Return")
-        return jsonify(
+
+        return JSONResponse(
             {
                 'id': ids,
                 'predictions': res,
@@ -89,14 +175,10 @@ def predict_prob2():
 
     except Exception as e:
         logger.error(e)
-        return jsonify(
+        return JSONResponse(
             {
                 'id': ids,
                 'predictions': res,
                 'drift': 0
             }
         )
-
-
-if __name__ == '__main__':
-    app.run()
